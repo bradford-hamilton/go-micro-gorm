6e3773cad2a3af127656ce6dc7cb1df25b432b0d
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-02-15 20:29:13 -0700

    Add and migrate a new user model/table to the db

--- internal/db/db.go
+++ internal/db/db.go
@@ -10,11 +10,28 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+// Emedding `gorm.Model` adds the following conventional db fields:
+//		ID        uint `gorm:"primary_key"`
+// 		CreatedAt time.Time
+// 		UpdatedAt time.Time
+// 		DeletedAt *time.Time `sql:"index"`
+//
+// By keeping the `DeletedAt` field, gorm will mark records with a
+// timestamp instead to destroying them
+
 // Message defines the model for a Message in the system and embeds a gorm model
 type Message struct {
 	gorm.Model
 	MsgType string
 	Msg     string
+}
+
+// User defines the model for a User in the system and embeds a gorm model
+type User struct {
+	gorm.Model
+	FirstName   string
+	LastName    string
+	MailingList bool `gorm:"default:false"`
 }
 
 // New creates new connection to the database and returns it to caller
@@ -47,7 +64,7 @@ func New() (*gorm.DB, error) {
 	}
 
 	// Migrate the schema
-	db.AutoMigrate(&Message{})
+	db.AutoMigrate(&Message{}, &User{})
 
 	// Seed the db when in development
 	if env := os.Getenv("GO_MICRO_GORM_ENV"); env == "development" {
@@ -66,4 +83,14 @@ func seedDatabase(db *gorm.DB) {
 	db.Create(&Message{MsgType: "Private", Msg: "I'm another private message."})
 	db.Create(&Message{MsgType: "Protected", Msg: "Hey, I'm a protected message!"})
 	db.Create(&Message{MsgType: "Protected", Msg: "I'm another protected message."})
+}
+
+// ErrSliceToString is a small helper function for now to join a slice of errors, which
+// is the type that is returned from a gorm GetErrors() call.
+func ErrSliceToString(errs []error) []string {
+	var errStrings []string
+	for _, e := range errs {
+		errStrings = append(errStrings, e.Error())
+	}
+	return errStrings
 }
